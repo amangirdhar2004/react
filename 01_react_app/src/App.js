7ab25a61793f5b4aba96c6fc99a51520a62476ab
Aman Girdhar <amangirdhar2004@gmail.com>
2025-02-26 16:20:14 +0530

    react introduced

---
+++ 01_react_app/src/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+
+function App() {
+  return (
+    <h1> Aman Girdhar will learn React Soon</h1>
+  );
+}
+
+export default App;
---
+++ 01_vite_react/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    
+      <h1>Aman Girdhar learning React now</h1>
+    
+  )
+}
+
+export default App
